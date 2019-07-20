43862cf54de5951c0214679b9e8bc26666755314
derrick.xing <xsy961127@buaa.edu.cn>
2019-07-20 21:54:02 +0800

    20190720更新

--- 35数组中的逆序对 .py
+++ 35数组中的逆序对 .py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+# 归并排序需要重点掌握
 """
 数组中的逆序
 题目描述：在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。
@@ -23,7 +24,41 @@ class Solution:
             count += data.index(i)
             data.pop(data.index(i))
         return count
+    def InversePairs2(self, data):
+        # 归并排序法
+        if not data:
+            return 0
+        temp = data.copy()
+        return self.helper(data,temp,0,len(data)-1)
+    def helper(self,data,temp,start,end):
+        if start==end:
+            temp[start] = data[start]
+            return 0
+        mid = (start + end)//2
+        left = self.helper(data,temp,start,mid)
+        right = self.helper(data,temp,mid+1,end)
+        i,j = start,mid+1
+        index,count = start,0
+        while i<=mid and j<=end:
+            if data[i] <= data[j]:
+                temp[index] = data[i]
+                i+=1
+            else:
+                temp[index] = data[j]
+                j+=1
+                count+=1
+            index += 1
+        while i<= mid:
+            temp[index] = data[i]
+            i+=1
+            index+=1
+        while j<= end:
+            temp[index] = data[j]
+            j+=1
+            index+=1
+        return left+count+right
+
 if __name__=='__main__':
     Solution = Solution()
-    a = Solution.InversePairs([2,1,1,3,3,3])
+    a = Solution.InversePairs2([2,1,1,3,3,3])
     print(a)
